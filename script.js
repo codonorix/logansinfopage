ed815b860d0b1bd33a11bd8bccf3fee19136240f
codonorix <logan.meadows20@gmail.com>
2022-11-14 19:33:13 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,37 @@
+class projects()
+
+/*
+        <div class="row justify-content-center">
+            <div class="col-md-3" id="project-one">
+                <a href="#">
+                    <h5>Project Name</h5>
+                </a>
+                <br>
+                <p>Project desription</p>
+            </div>
+        </div>
+ */
+
+
+
+// document.addEventListener('contextmenu', function (e) {
+//     e.preventDefault()
+//
+//     let menu = document.getElementById('menu')
+//
+//     menu.style.transition = "all 1s"
+//
+//     menu.style.display = 'none'
+//     menu.style.position = 'absolute';
+//     menu.style.display = 'block'
+//     menu.style.opacity = parseFloat(menu.style.opacity) + 0.1;
+//
+//
+//     menu.style.left = e.clientX + 'px'
+//     menu.style.top = e.clientY + 'px'
+// })
+//
+// document.addEventListener('click', function (e) {
+//     let menu = document.getElementById('menu')
+//     menu.style.display = 'none'
+// })
